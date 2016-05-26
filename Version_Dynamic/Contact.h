b606fde506b40f7d9ea8023a4f069a40638b2ad7
urhosts <hosts@gsxio.com>
2016-05-26 23:14:14 +0800

    Version_Dynamic

---
+++ Version_Dynamic/Contact.h
@@ -0,0 +1,57 @@
+#pragma 
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+#ifndef __CONTACT_H__
+#define __CONTACT_H__
+
+enum OP
+{
+	EXIT,
+	ADD,
+	DEL,
+	MODIFY,
+	SEARCH,
+	DISPLAY,
+	SORT,
+	CLEAR,
+};
+
+#define NAME_MAX 20
+#define SEX_MAX 5
+#define TELE_MAX 12
+#define ADDR_MAX 25
+#define INIT_CAPACITY 2//通讯录初始大小
+#define INC_CAPACITY 2//每次扩容的数量
+
+typedef struct PeoInfo
+{
+	char name[NAME_MAX];
+	int age;
+	char sex[SEX_MAX];
+	char tele[TELE_MAX];
+	char address[ADDR_MAX];
+}PeoInfo;
+
+typedef struct Contact
+{
+	PeoInfo *data;
+	int size;
+	int capacity;
+}Contact, *Pcon;
+
+void _init_contact(Pcon pcon);
+void _destory_contact(Pcon pcon);
+void _add_contact(Pcon pcon);
+void _display_contact(Pcon pcon);
+void _del_contact(Pcon pcon);
+void _search_contact(Pcon pcon);
+void _modify_contact(Pcon pcon);
+void _sort_contact(Pcon pcon);
+void _clear_contact(Pcon pcon);
+
+#endif //__CONTACT_H__
